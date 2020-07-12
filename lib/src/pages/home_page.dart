23e3bf546cc711dc660f3d5a9b86a645e9f5eeaa
Jairo Carmona <jairo.carmona@datapeers.co>
2020-07-12 11:49:26 -0500

    Delete test and added button in app

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
 
+  final estiloTexto = new TextStyle( fontSize: 25 );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,9 +16,16 @@ class HomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text('Número de clicks!'),
+            Text('Número de clicks!', style: estiloTexto),
+            Text('0', style: estiloTexto),
           ],
         )
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon( Icons.add ),
+        onPressed: () {
+          print('Hola Mundo!');
+        }, 
       ),
     );
   }
